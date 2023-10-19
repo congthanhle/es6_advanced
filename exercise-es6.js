66996ff53e66ba1ae3b09e57c0ca5d28f90f5bfd
congthanhle <lecongthanhbaoloclamdong@gmail.com>
2023-10-19 10:40:02 +0700

    fix push data using ES6

--- exercise-es6.js
+++ exercise-es6.js
@@ -7,74 +7,36 @@ const result = data.reduce((acc, item) => {
   if (!acc[topic]) {
     acc[topic] = [];
   }
-/* 
-  "1":[
 
-  ]
-*/
   let topicObj = acc[topic].find((topicItem) => topicItem.id === topic);
   if (!topicObj) {
     topicObj = {
       id: topic,
       lesson: [],
     };
-    acc[topic].push(topicObj);
+    acc[topic] = [...acc[topic], topicObj];
   }
-/*
-   "1":[
-      "id": 1,
-      "lesson": []
-   ]
-*/
-  let lessonObj = topicObj.lesson.find((lessonItem) => lessonItem.id === lesson);
+
+  let lessonObj = topicObj.lesson.find(
+    (lessonItem) => lessonItem.id === lesson
+  );
   if (!lessonObj) {
     lessonObj = {
       id: lesson,
       cando: [],
     };
-    topicObj.lesson.push(lessonObj);
+    topicObj.lesson = [...topicObj.lesson, lessonObj];
   }
-/*
-   "1":[
-    {
-      "id": 1,
-      "lesson": [
-        {
-          "id": 1,
-          "cando": []
-        }
-      ]
-    } 
-   ]
-*/
+
   let candoObj = lessonObj.cando.find((candoItem) => candoItem.id === cando);
   if (!candoObj) {
     candoObj = {
       id: cando,
       step: [],
     };
-    lessonObj.cando.push(candoObj);
+    lessonObj.cando = [...lessonObj.cando, candoObj];
   }
-/*
-   "1":[
-    {
-      "id": 1,
-      "lesson": [
-        {
-          "id": 1,
-          "cando": [
-            {
-              "id": 1,
-              "step": [
 
-              ]
-            }
-          ]
-        }
-      ]
-    } 
-   ]
-*/
   let stepObj = candoObj.step.find((stepItem) => stepItem.id === step);
   if (!stepObj) {
     stepObj = {
@@ -83,65 +45,21 @@ const result = data.reduce((acc, item) => {
       typeEnd: [],
       typeNormal: [],
     };
-    candoObj.step.push(stepObj);
+    candoObj.step = [...candoObj.step, stepObj];
   }
-/*
-   "1":[
-    {
-      "id": 1,
-      "lesson": [
-        {
-          "id": 1,
-          "cando": [
-            {
-              "id": 1,
-              "step": [
-                "id": 1,
-                "typeStart": [],
-                "typeEnd": [],
-                "typeNormal": []
-              ]
-            }
-          ]
-        }
-      ]
-    } 
-   ]
-*/
+
   switch (progress_type) {
     case "lesson_start":
-      stepObj.typeStart.push({ content });
+      stepObj.typeStart = [...stepObj.typeStart, {content}]
       break;
     case "lesson_end":
-      stepObj.typeEnd.push({ content });
+      stepObj.typeEnd = [...stepObj.typeEnd, {content}]
       break;
     default:
-      stepObj.typeNormal.push({ content });
+      stepObj.typeNormal = [...stepObj.typeNormal, {content}]
       break;
   }
-/*
-   "1":[
-    {
-      "id": 1,
-      "lesson": [
-        {
-          "id": 1,
-          "cando": [
-            {
-              "id": 1,
-              "step": [
-                "id": 1,
-                "typeStart": [ content],
-                "typeEnd": [],
-                "typeNormal": [content]
-              ]
-            }
-          ]
-        }
-      ]
-    } 
-   ]
-*/
+
   return acc;
 }, {});
 
